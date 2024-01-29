fd5ed61d8acf9d3a54b5e6213668d25ab1cecd03
Neeraj Jawahirani <neerajjawahirani@gmail.com>
2024-01-29 18:52:29 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,14 +1,14 @@
 def question6_1():
     answer = {
-        'equal_width': ['Change', 0],
-        'equal_frequency': ['Change', 0]
+        'equal_width': ['change', 0],
+        'equal_frequency': ['change', 0]
     }
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['Change', 2],
-        'equal_frequency': ['Change', 2]
+        'equal_width': ['change', 2],
+        'equal_frequency': ['change', 2]
     }
     return answer
 
